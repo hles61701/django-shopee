09925067e72c5666d7aab0de4bb50cb3fdad753b
hles61701 <hles61701@gmail.com>
2021-01-29 00:43:28 +0800

    建立models

--- shopee/models.py
+++ shopee/models.py
@@ -4,3 +4,16 @@ from datetime import datetime
 import requests
 
 # Create your models here.
+class shopee:
+    def __int__(self, product_name):
+        self.product_name = product_name
+
+    def scrape(self):
+        result = []
+
+        if self.product_name:
+            resp = requests.get(f'https://shopee.tw/api/v2/search_items/?by=relevancy&keyword={self.product_name}&limit=20&newest=0&order=desc&page_type=search&version=2')
+            items = resp.json['items']
+        return items
+
+            
--- shopee/views.py
+++ shopee/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import shopee
 
 # Create your views here.
 def shopee(request):
