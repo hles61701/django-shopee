45f57dafb6419f6ed3b1386529ed6d7b972f06b0
hles61701 <hles61701@gmail.com>
2021-01-28 15:35:50 +0800

    建立urls 建立templates

--- shopee/models.py
+++ shopee/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from bs4 import BeautifulSoup as bs
+from datetime import datetime
+import requests
 
 # Create your models here.
--- shopee/views.py
+++ shopee/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shopee(request):
+    return render(request, 'shopee/shopee.html')
